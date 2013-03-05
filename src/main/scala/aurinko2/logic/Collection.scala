2f066f5105e352771a6f788c85dee1ff6c5ac8f5
HouzuoGuo <guohouzuo@gmail.com>
2013-03-05 16:52:34 +1100

    collection insert

--- src/main/scala/aurinko2/logic/Collection.scala
+++ src/main/scala/aurinko2/logic/Collection.scala
@@ -5,7 +5,6 @@ import java.io.RandomAccessFile
 import java.nio.file.Paths
 import java.util.concurrent.locks.ReentrantLock
 import java.util.logging.Logger
-
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.Await
@@ -15,7 +14,6 @@ import scala.xml.Elem
 import scala.xml.NodeSeq
 import scala.xml.XML
 import scala.xml.XML.loadString
-
 import aurinko2.io.SimpleIO.spit
 import aurinko2.storage.{ Collection => CollFile }
 import aurinko2.storage.CollectionDelete
@@ -24,12 +22,11 @@ import aurinko2.storage.CollectionRead
 import aurinko2.storage.CollectionUpdate
 import aurinko2.storage.Hash
 import aurinko2.storage.HashPut
-import aurinko2.storage.HashPut
 import aurinko2.storage.HashWork
 import aurinko2.storage.Log
 import aurinko2.storage.LogInsert
-import aurinko2.storage.LogInsert
 import aurinko2.storage.Output
+import aurinko2.storage.HashRemove
 
 object Collection {
   val LOG = Logger.getLogger(classOf[Collection].getName())
@@ -64,15 +61,16 @@ class Collection(val path: String) {
   if (!(configFile.canRead() && configFile.canWrite()))
     throw new Exception(s"Config file $configFilename is not RW to you")
 
+  // Parse configuration file
   var config: Elem = null
   try {
     config = XML.loadFile(configFile)
   } catch {
     case e: org.xml.sax.SAXParseException =>
-      Collection.LOG.severe(s"XML parser cannot parse $path/config, manually repair the collection?")
+      throw new Exception(s"XML parser cannot parse $path/config, manually repair the collection?")
   }
 
-  // Load hash indexes
+  // Parse collection indexes
   val hashes = new HashMap[List[String], Tuple2[String, Hash]]
   if (config != null)
     (config \ "hashes").foreach { hash =>
@@ -100,6 +98,7 @@ class Collection(val path: String) {
       }
     }
 
+  // Open data files
   val collection = new CollFile(new RandomAccessFile(Paths.get(path, "data").toString, "rw").getChannel())
   val log = new Log(new RandomAccessFile(Paths.get(path, "log").toString, "rw").getChannel())
 
@@ -110,11 +109,7 @@ class Collection(val path: String) {
     if (hashes.contains(path))
       throw new Exception(s"Collection ${this.path} already has $path indexed")
 
-    val last = path(path.size - 1)
-    if (last.length() > 100)
-      throw new Exception(s"Last path segment ($last) is too long (> 100 characters)")
-
-    val filename = last + System.nanoTime().toString
+    val filename = path(path.size - 1).substring(100) + System.nanoTime().toString
     hashes += ((path, (filename, new Hash(new RandomAccessFile(filename.toString, "rw").getChannel(), bits, perBucket))))
     saveConfig()
   }
@@ -139,9 +134,9 @@ class Collection(val path: String) {
     if (path.size == 0) {
       return (nodes map { node =>
         if (node.child.size > 0 && node.child(0).isInstanceOf[Elem])
-          node.toString // Indexed XML element
+          node.toString // Index XML element
         else
-          node.child.mkString("") // Indexed XML non-element
+          node.child.mkString("") // Index XML non-element
       }).toList
     } else {
       val ret = new ListBuffer[String]
@@ -163,61 +158,53 @@ class Collection(val path: String) {
     return loadString(new String(work.data.data))
   }
 
+  def indexDoc(doc: Elem, id: Int): List[Promise[HashWork]] = {
+    val promises = new ListBuffer[Promise[HashWork]]
+    hashes foreach { index =>
+      for (toIndex <- getIn(doc, index._1))
+        promises += index._2._2.offer(HashPut(toIndex.hashCode(), id))
+    }
+    return promises.toList
+  }
+
+  def unindexDoc(doc: Elem, id: Int): List[Promise[HashWork]] = {
+    val promises = new ListBuffer[Promise[HashWork]]
+    hashes map { index =>
+      for (toIndex <- getIn(doc, index._1))
+        promises += index._2._2.offer(HashRemove(toIndex.hashCode(), 1, (_, value) => value == id))
+    }
+    return promises.toList
+  }
+
   /** Insert a document, return its ID. */
   def insert(doc: Elem): Int = {
-    val docBytes = doc.toString.getBytes
-    val colInsert = CollectionInsert(docBytes, new Output[Int](0))
-    val logInsert = LogInsert(<i>{ doc }</i>.toString.getBytes, new Output[Int](0))
-    val hashCodes = for (index <- hashes) yield (index, getIn(doc, index._1).hashCode)
-    write.lock()
-    try {
 
-      if (hashes.size != hashCodes.size)
-        return insert(doc)
+    // Insert to collection
+    val colInsert = CollectionInsert(doc.toString.getBytes, new Output[Int](0))
+    val colPromise = collection.offer(colInsert)
+    Await.result(colPromise.future, Int.MaxValue nanosecond)
 
-      // Have to wait for collection insert to work out index values
-      Await.result(collection.offer(colInsert).future, Int.MaxValue nanosecond)
-      log.offer(logInsert)
-      hashCodes foreach { h => h._1._2._2.offer(HashPut(h._2, colInsert.pos.data)) }
+    // Insert to log and indexes
+    val logPromise = log.offer(LogInsert(<i>{ doc }</i>.toString.getBytes, new Output[Int](0)))
+    val indexPromises = indexDoc(doc, colInsert.pos.data)
 
-      return colInsert.pos.data
-
-    } finally {
-      write.unlock()
+    // Wait for log and indexes
+    (logPromise +: indexPromises) foreach { promise =>
+      Await.result(promise.future, Int.MaxValue nanosecond)
     }
+
+    return colInsert.pos.data
   }
 
   /** Update a document given its ID and new document element, return updated document's ID. */
   def update(id: Int, doc: Elem): Int = {
-    val docBytes = doc.toString.getBytes
-    val colUpdate = CollectionUpdate(id, docBytes, new Output[Int](0))
-    val logInsert = LogInsert(<u><id>{ id }</id><doc>{ doc }</doc></u>.toString.getBytes, new Output[Int](0))
-    val hashCodes = for (index <- hashes) yield (index, getIn(doc, index._1).hashCode)
-    write.lock()
-    try {
-      if (hashes.size != hashCodes.size)
-        return update(id, doc)
-
-      Await.result(collection.offer(colUpdate).future, Int.MaxValue nanosecond)
-      log.offer(logInsert)
-
-    } finally {
-      write.unlock()
-    }
+    val oldDoc = read(id)
+    return 0
   }
 
   /** Delete a document given its ID. */
   def delete(id: Int) {
-    val work = CollectionDelete(id)
-    write.lock()
-    try {
-      val p = collection.offer(work)
-      if (!wait)
-        return
-      Await.result(p.future, Int.MaxValue nanosecond)
-    } finally {
-      write.unlock()
-    }
+    return 0
   }
 
   // Configuration and other
@@ -225,7 +212,7 @@ class Collection(val path: String) {
   def saveConfig() {
 
     // Copy existing config to .bak
-    val source = scala.io.Source.fromFile("file.txt")
+    val source = scala.io.Source.fromFile(Paths.get(path, "config").toString)
     spit(Paths.get(path, "config.bak").toString, Seq(source.mkString), false)
     source.close()
 

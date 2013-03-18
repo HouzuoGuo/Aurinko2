e26a364b317614f47007ac1715921cff34c88a1f
HouzuoGuo <guohouzuo@gmail.com>
2013-03-18 20:42:12 +1100

    collection management and document management

--- src/main/scala/net/houzuo/aurinko2/Worker.scala
+++ src/main/scala/net/houzuo/aurinko2/Worker.scala
@@ -7,7 +7,6 @@ import java.io.PrintWriter
 import java.net.Socket
 import java.util.logging.Logger
 
-import scala.annotation.migration
 import scala.collection.mutable.ListBuffer
 import scala.xml.Elem
 import scala.xml.NodeSeq.seqToNodeSeq
@@ -19,15 +18,17 @@ import net.houzuo.aurinko2.logic.Database
 
 object Worker {
   val LOG = Logger.getLogger(classOf[Worker].getName())
+  val MAX_REQUEST_LENGTH = 20000000 // Max request length in bytes 
 }
 
 class Worker(val db: Database, val sock: Socket) {
-  val in = new BufferedReader(new InputStreamReader(sock.getInputStream()))
-  val out = new PrintWriter(sock.getOutputStream(), true)
+  private val in = new BufferedReader(new InputStreamReader(sock.getInputStream()))
+  private val out = new PrintWriter(sock.getOutputStream(), true)
 
   // Process requests until peer closes the connection
   try {
     val lines = new ListBuffer[String]
+    var reqLength = 0
     while (true)
       this.synchronized {
         val line = in.readLine()
@@ -37,13 +38,27 @@ class Worker(val db: Database, val sock: Socket) {
         // Accumulate input until <go /> is given, which marks end of request
         val trimmed = line.trim().toLowerCase()
         if ("<go/>".equals(trimmed) || "<go />".equals(trimmed) || "<go></go>".equals(trimmed))
-          try {
-            go(loadString(lines.mkString("")))
-          } catch {
-            case e: SAXParseException => out.println(<err>Unable to parse request as XML document</err>)
-          } finally { lines.clear() }
-        else
-          lines += line
+          if (lines.size == 0)
+            respond { Some(<done/>) }
+          else
+            try {
+              go(loadString(lines.mkString("")))
+            } catch {
+              case e: SAXParseException => out.println(<err>Unable to parse request as XML document</err>)
+              case e: Exception =>
+                respond { Some(<err>{ e.getMessage }</err>) }
+                Worker.LOG.severe(s"${e.getMessage}: \n${e.getStackTraceString}")
+            } finally { lines.clear(); reqLength = 0 }
+        else {
+          reqLength += line.length()
+          if (reqLength > Worker.MAX_REQUEST_LENGTH) {
+            respond { Some(<err>Request is too long</err>) }
+            lines.clear()
+            reqLength = 0
+          } else
+            lines += line
+
+        }
       }
   } catch {
     case e: IOException => Worker.LOG.warning(s"${sock.getRemoteSocketAddress().toString()} disconnected")
@@ -60,7 +75,7 @@ class Worker(val db: Database, val sock: Socket) {
   }
 
   /** Print response to output. */
-  def respond(res: => Option[Elem]) {
+  private def respond(res: => Option[Elem]) {
     try {
       res match {
         case Some(thing) => out.println(thing)
@@ -74,32 +89,114 @@ class Worker(val db: Database, val sock: Socket) {
   }
 
   /** Process request. */
-  def go(req: Elem) {
-    req match {
-
-      // Ping server
-      case <ping/> => respond { Some(<pong/>) }
+  private def go(req: Elem) {
+    req.label.toLowerCase match {
 
       // Get all collection names
-      case <all/>  => respond { Some(<all>{ for (col <- db.all) yield <col>{ col }</col> }</all>) }
+      case "all" => respond { Some(<all>{ for (col <- db.all) yield <col>{ col }</col> }</all>) }
 
-      // Get all collection data file queues' size
-      case <load/> => respond {
-        Some(<load>
-               {
-                 for (col <- db.all) yield <col name={ col }>{
-                   for (thing <- db.get(col).load) yield <queue a={ thing._1.toString }>{ thing._2 }</queue>
-                 }</col>
-               }
-             </load>)
+      // Get IO queue length on all collections
+      case "load" => respond {
+        Some(<load>{
+          for (col <- db.all) yield <col name={ col }>{
+            for (thing <- db.get(col).load) yield <queue name={ thing._1.toString }>{ thing._2 }</queue>
+          }</col>
+        }</load>)
       }
 
-      // Create a collection
-      case <create/> => respond {
+      // Create collection
+      case "create" => respond {
         req.attribute("name") match {
           case Some(name) =>
             db.create(name.text); None
-          case None => Some(<err>Please specify new collection name in "name" attribute</err>)
+          case None => Some(<err>Please specify collection name in "name" attribute</err>)
+        }
+      }
+
+      // Rename collection
+      case "rename" => respond {
+        req.attribute("old") match {
+          case Some(oldName) =>
+            req.attribute("new") match {
+              case Some(newName) =>
+                db.rename(oldName.text, newName.text); None
+              case None => Some(<err>Please spicify new collection in "new" attribute</err>)
+            }
+          case None => Some(<err>Please specify old collection in "old" attribute</err>)
+        }
+      }
+
+      // Drop collection
+      case "drop" => respond {
+        req.attribute("name") match {
+          case Some(name) =>
+            db.drop(name.text); None
+          case None => Some(<err>Please specify collection name in "name" attribute</err>)
+        }
+      }
+
+      // Repair collection
+      case "repair" => respond {
+        req.attribute("name") match {
+          case Some(name) =>
+            db.repair(name.text); None
+          case None => Some(<err>Please specify collection name in "name" attribute</err>)
+        }
+      }
+
+      // Insert documents
+      case "insert" => respond {
+        req.attribute("col") match {
+          case Some(colName) =>
+            val col = db.get(colName.text)
+            Some(<inserted>{ for (doc <- req.child) yield <id>{ col.insert(doc.asInstanceOf[Elem]) }</id> }</inserted>)
+          case None => Some(<err>Please specify collection name in "col" attribute</err>)
+        }
+      }
+
+      // Update documents
+      case "update" => respond {
+        req.attribute("col") match {
+          case Some(colName) =>
+            val col = db.get(colName.text)
+            Some(<updated>{
+              for (update <- req.child)
+                yield update.attribute("id") match {
+                case Some(oldID) =>
+                  <id old={ oldID }>{ col.update(oldID.text.toInt, update.child(0).asInstanceOf[Elem]).get }</id>
+                case None => throw new Exception("Please specify ID of all documents to update")
+              }
+            }</updated>)
+          case None => Some(<err>Please specify collection name in "col" attribute</err>)
+        }
+      }
+
+      // Delete documents
+      case "delete" => respond {
+        req.attribute("col") match {
+          case Some(colName) =>
+            val col = db.get(colName.text)
+            for (toDelete <- req.child)
+              col.delete(toDelete.text.toInt)
+            None
+          case None => Some(<err>Please specify collection name in "col" attribute</err>)
+        }
+      }
+
+      // Get all documents
+      case "findall" => respond {
+        req.attribute("col") match {
+          case Some(colName) =>
+            val col = db.get(colName.text)
+            Some(<collection>{
+              for (
+                docID <- req.attribute("limit") match {
+                  case Some(number) => col.all.take(number.text.toInt)
+                  case None         => col.all
+                }
+              ) yield col.read(docID).get
+            }</collection>)
+          case None => Some(<err>Please specify collection name in "col" attribute</err>)
         }
       }
     }

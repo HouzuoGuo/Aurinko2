e1ca0394b77364c405e9aeb1507476a744a3e2c5
HouzuoGuo <guohouzuo@gmail.com>
2013-03-25 21:01:38 +1100

    more query logics implemented

--- src/main/scala/net/houzuo/aurinko2/logic/Query.scala
+++ src/main/scala/net/houzuo/aurinko2/logic/Query.scala
@@ -1,7 +1,6 @@
 package net.houzuo.aurinko2.logic
 
 import java.util.logging.Logger
-
 import scala.collection.mutable.HashSet
 import scala.collection.mutable.SetBuilder
 import scala.concurrent.Await
@@ -10,9 +9,9 @@ import scala.concurrent.duration.DurationInt
 import scala.xml.Elem
 import scala.xml.Node
 import scala.xml.NodeSeq
-
 import net.houzuo.aurinko2.storage.HashGet
 import net.houzuo.aurinko2.storage.Output
+import net.houzuo.aurinko2.storage.HashGetAll
 
 object Query {
   val LOG = Logger getLogger classOf[Query].getName
@@ -58,7 +57,7 @@ class Query(val col: Collection) {
     {
       // Index scan when possible
       if (col.hashes.contains(path)) {
-        Query.LOG fine s"Index scan ${path}"
+        Query.LOG fine s"Index scan eq ${path}"
         val scan = HashGet(to.toString hashCode, limit, (_1, id) => {
           col.read(id) match {
             case Some(doc) => Collection.getIn(doc.asInstanceOf[NodeSeq], path) contains to
@@ -69,7 +68,7 @@ class Query(val col: Collection) {
         scan.result.data map (_._2)
       } else {
         // Otherwise, read each document and compare
-        Query.LOG fine s"Inefficient! Collection scan ${path}"
+        Query.LOG fine s"Inefficient! Collection scan eq ${path}"
         col.all filter { id =>
           col.read(id) match {
             case Some(doc) => Collection.getIn(doc.asInstanceOf[NodeSeq], path) contains to
@@ -85,17 +84,80 @@ class Query(val col: Collection) {
     } take limit toSet
   }
 
+  /**
+   * Find documents which contain the path using look-up (very efficient) or collection scan (inefficient).
+   * Support "limit" and "skip" parameters as well, for example:
+   * <has limit="1" skip="1">
+   *   <in>
+   *     <path>sales</path>
+   *     <path>product1</path>
+   *   </in>
+   * </has>
+   */
+  def qhas(op: Node): Set[Int] = {
+    val children = op.child.map { c => c.label -> c } toMap
+    val path = children get "in" match {
+      case Some(in) => in.child map (_.text) toList
+      case None     => throw new Exception("Please specify matching path in <in></in>")
+    }
+    val limit = op attribute "limit" match {
+      case Some(number) => number.text toInt
+      case None         => Int.MaxValue
+    }
+    {
+      // Index scan when possible
+      if (col.hashes.contains(path)) {
+        Query.LOG fine s"Index scan path ${path}"
+        val scan = HashGetAll(new Output[List[Tuple2[Int, Int]]](null))
+        sync(col.hashes.get(path).get._2.offer(scan))
+        scan.result.data map (_._2)
+      } else {
+        // Otherwise, read each document and "get-in"
+        Query.LOG fine s"Inefficient! Collection scan path ${path}"
+        col.all filter { id =>
+          col.read(id) match {
+            case Some(doc) => !Collection.getIn(doc.asInstanceOf[NodeSeq], path).isEmpty
+            case None      => false
+          }
+        }
+      }
+    } drop {
+      op attribute "skip" match {
+        case Some(number) => number.text.toInt
+        case None         => 0
+      }
+    } take limit toSet
+  }
+
   def eval(query: Node): Set[Int] = {
+    val result = new HashSet[Int]
     for (op <- query.child) {
-      op.label match {
-        case "eq"    => return qeq(op)
-        case "has"   =>
-        case "all"   => return col.all toSet
-        case "union" => return { for (littleOp <- op.child) yield eval(littleOp) }.flatten toSet
-        case "diff"  =>
-        case _       => throw new Exception(s"Unknown query operation ${op.label}")
+      result ++= {
+        op.label match {
+          case "eq"  => qeq(op)
+          case "has" => qhas(op)
+          case _ =>
+            {
+              op.label match {
+                case "all"   => col.all toSet
+                case "union" => { for (littleOp <- op.child) yield eval(littleOp) }.flatten.toSet
+                case "diff"  => { for (littleOp <- op.child) yield eval(littleOp) }.reduceLeft(_ diff _)
+                case _       => throw new Exception(s"Unknown query operation '${op.label}'")
+              }
+            } drop {
+              op attribute "skip" match {
+                case Some(number) => number.text.toInt
+                case None         => 0
+              }
+            } take {
+              op attribute "limit" match {
+                case Some(number) => number.text toInt
+                case None         => Int.MaxValue
+              }
+            }
+        }
       }
     }
-    return Set(0)
+    result toSet
   }
 }
